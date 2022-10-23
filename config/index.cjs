23a2647ce5a7042557932f87c1704f4c072aa822
lgwsgc <lgwny0120@163.com>
2022-10-23 16:25:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -48,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'ohr5j6DcC0yEtW0bAv3pcZmWoeeI',
+  CALLBACK_TEMPLATE_ID: 'BtkOmMw_Lrlqh63MnaxtyZpKIuoRy6lCXO5W9TkkBMc',
 
   CALLBACK_USERS: [
     {
