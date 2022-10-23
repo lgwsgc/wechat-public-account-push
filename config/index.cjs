0827d14f1bb595a813f1d607944ad25e4b86c14e
lgwsgc <104776062+lgwsgc@users.noreply.github.com>
2022-10-23 14:59:43 +0800

    Update index.cjs
    因注释东西太多，故暂时删掉些多余的内容，

--- config/index.cjs
+++ config/index.cjs
@@ -31,27 +31,17 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '宝贝', year: '1999', date: '04-20',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-//         {
-//           type: '生日', name: '李四', year: '1996', date: '09-31',
-//         },
-//         {
-//           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-//         },
-      ],
+               ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-//         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-    },
   CALLBACK_TEMPLATE_ID: 'YnHpigA9oJlva3niHtEhvFS-UOMfCw40RnA6skzN79E',
 
   CALLBACK_USERS: [
