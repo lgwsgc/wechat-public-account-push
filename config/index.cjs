38b1e63f976d6d29e57eb7ea99a602f76e349db8
lgwsgc <lgwny0120@163.com>
2022-10-24 10:32:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohr5j6DcC0yEtW0bAv3pcZmWoeeI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wr1zS5U1zwGC7wRxoASApJY8dBXpwzTfr32p_PvaKk0',
+      useTemplateId: 'olzd4P3HBFiPIEpY0xaonVxz1JukFAsi3ub4eUS3YQM',
       province: '浙江',
       city: '宁波',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
