920d21a30a184a7024a9817fbdefa729d22d2673
lgwsgc <lgwny0120@163.com>
2022-10-24 09:12:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,13 +67,13 @@ const USER_CONFIG = {
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'BtkOmMw_Lrlqh63MnaxtyZpKIuoRy6lCXO5W9TkkBMc',
-  // CALLBACK_USERS: [
-  //   {
-  //     name: '自己',
-  //     // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-  //     id: 'ohr5j6DcC0yEtW0bAv3pcZmWoeeI',
-  //   }
-  // ],
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ohr5j6DcC0yEtW0bAv3pcZmWoeeI',
+    }
+  ],
 
 }
 
