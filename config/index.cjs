30a71221a1adfbf2b70353c38bfe930b3703052c
lgwsgc <lgwny0120@163.com>
2022-10-26 09:36:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,6 +21,29 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '琳琳',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ohr5j6K6utUUEG1J7BFxDYi7Cnjg',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'IflAMe2VI0Mgpwg5oxz1a1ssuDCHfd_dOKUNwsKcQLo',
+      province: '浙江',
+      city: '宁波',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '4-23',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '琳琳', year: '1999', date: '03-19',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-10-26' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '琳威',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohr5j6DcC0yEtW0bAv3pcZmWoeeI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'IflAMe2VI0Mgpwg5oxz1a1ssuDCHfd_dOKUNwsKcQLo',
