2c7387d8f9e56803c4ebb31a862e6d1d6f5aec9c
lgwsgc <lgwny0120@163.com>
2022-10-26 08:42:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,21 +23,21 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohr5j6DcC0yEtW0bAv3pcZmWoeeI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'olzd4P3HBFiPIEpY0xaonVxz1JukFAsi3ub4eUS3YQM',
+      useTemplateId: '	7mSK6RZolq-u5eUJ608GVLQRGONc7c4jgntzKSYV_iE',
       province: '浙江',
       city: '宁波',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '8-23',
+      horoscopeDate: '4-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '琳琳', year: '1999', date: '04-20',
+          type: '*生日', name: '琳琳', year: '1999', date: '03-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-10-26' },
       ],
     },
     {
